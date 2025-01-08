a6844e36614abf5716c9a88187e3749b44cb7534
Michael Frenkel <michaelfrenkel@coherentsolutions.com>
2025-01-08 15:12:40 +0100

    MainPage.xaml Warning XC0025

---
+++ src/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace DataTypeInBindingExtension;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private string _btnText = "Click me";
+
+	public string BtnText
+	{
+		get => _btnText;
+		set
+		{
+			if (_btnText != value)
+			{
+				_btnText = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			BtnText = $"Clicked {count} time";
+		else
+			BtnText = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(BtnText);
+	}
+}
+
